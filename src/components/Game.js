f87f24a8fa6d5fb703ff35e2425a9c64778a260f
“winiciuskyutoku“ <winicius123098@gmail.com>
2023-02-27 12:14:32 -0300

    Data-test imagem implemented

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,7 @@ function Game(props) {
    
     return (
         <div className="game">
-            <img src={props.misses} />
+            <img src={props.misses} data-test="game-image" alt="boneco-forca"/>
             <div className="gamePanel">
                 <button onClick={props.onClick} disabled={props.isTrue === true ? false : true} data-test="choose-word">Escolher Palavra</button>
                 <h1 className={props.status} data-test="word">{props.isTure === true ? "" : props.correctWord}</h1>
